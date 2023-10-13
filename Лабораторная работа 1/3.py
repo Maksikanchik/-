fef0e15279e6ae5d7aca24a773c4adfa927ddab8
Maksikanchik <147877250+Maksikanchik@users.noreply.github.com>
2023-10-13 23:47:25 +0300

    Add files via upload

---
+++ Лабораторная работа 1/3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+Total = len(list_players)
+Team1 = list_players[:Total//2]
+Team2 = list_players[Total//2:]
+print(Team1)
+print(Team2)
